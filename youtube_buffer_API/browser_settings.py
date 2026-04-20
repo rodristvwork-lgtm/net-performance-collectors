da9cafd1d2506bf807194edd0af359df4a71d6ae
Rodriguez JHORDAN <rodriguez.jhordan@alten.it>
2026-04-20 10:25:27 +0200

    ota-gua - c5p5 -  settings driver for guacamole

--- youtube_buffer_API/browser_settings.py
+++ youtube_buffer_API/browser_settings.py
@@ -17,10 +17,12 @@ def get_driver_settings(url):
     srv = Service(executable_path=gecko_path)
 
     opt = webdriver.FirefoxOptions()
+    opt.add_argument("--headless")
     opt.add_argument("--no-sandbox")
     opt.add_argument("--disable-dev-shm-usage")
-    opt.add_argument("--display=:0")     # to visualize the video
-    
+    opt.add_argument("--width=1920")
+    opt.add_argument("--height=1080")
+
     opt.set_preference("media.ffmpeg.enabled", True)
     opt.set_preference("media.ffvpx.enabled", False)
     opt.set_preference("media.av1.enabled", False)
