50ab885cdd1cae837031532815cacbd0374777b9
Rodriguez JHORDAN <rodriguez.jhordan@alten.it>
2026-04-20 15:40:13 +0200

    Revert "ota-gua - c4p4 -  iperf launcher fixed"
    This reverts commit 8bae5b72b2d8eb5ca30e1024ba6176e6e0dff53f.

--- iperf_launcher.sh
+++ iperf_launcher.sh
@@ -4,7 +4,7 @@
 pgrep -f iperf.py | xargs -r kill -9 2>/dev/null
 
 # current directory
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+cd
 
 # Activate venv in current shell
 source "$SCRIPT_DIR/.venv/bin/activate"
